2892aba1204c3ad8c48be0b6a89d2fc2bc36944f
Paula Higa <paulahiga@msn.com>
2021-08-06 19:56:26 -0300

    implementação parcial da validaçao do nome

--- Formulario/ViewController.swift
+++ Formulario/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+        func validateName(name: String) ->Bool {
+              // Length be 18 characters max and 3 characters minimum, you can always modify.
+              let nameRegex = "^\\w{3,18}$"
+              let trimmedString = name.trimmingCharacters(in: .whitespaces)
+              let validateName = NSPredicate(format: "SELF MATCHES %@", nameRegex)
+              let isValidateName = validateName.evaluate(with: trimmedString)
+              return isValidateName
+           }
 
 }
 
